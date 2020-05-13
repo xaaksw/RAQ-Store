1d36d765dc6d720211357711b0d8817475367bd3
ahmed ashref <ahmed.ashref.sw@gmail.com>
2020-05-13 05:50:31 +0200

    EditCartModel

---
+++ RAQ-Store/RAQ-Store/Migrations/202005130340590_addProductFromTheSameType.cs
@@ -0,0 +1,24 @@
+﻿namespace RAQ_Store.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addProductFromTheSameType : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Cart", "product_id", "dbo.Product");
+            DropPrimaryKey("dbo.Cart");
+            AddPrimaryKey("dbo.Cart", new[] { "product_id", "added_at" });
+            AddForeignKey("dbo.Cart", "product_id", "dbo.Product", "id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Cart", "product_id", "dbo.Product");
+            DropPrimaryKey("dbo.Cart");
+            AddPrimaryKey("dbo.Cart", "product_id");
+            AddForeignKey("dbo.Cart", "product_id", "dbo.Product", "id");
+        }
+    }
+}
--- RAQ-Store/RAQ-Store/Models/Cart.cs
+++ RAQ-Store/RAQ-Store/Models/Cart.cs
@@ -10,15 +10,14 @@ namespace RAQ_Store.Models
     [Table("Cart")]
     public class Cart
     {
-        [Key]
+        [Key, Column(Order = 1)]
         [ForeignKey("Product")]
         public int product_id { get; set; }
         public Product Product  { get; set; }
 
 
-
+        [Key, Column(Order = 2)]
         [DataType(DataType.DateTime)]
-
         public DateTime added_at { get; set; }
 
 
