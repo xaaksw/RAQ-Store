3a5d8401e37313ffa96c6bf08bd46e6d142d6778
ahmed ashref <ahmed.ashref.sw@gmail.com>
2020-05-15 04:38:29 +0200

    adjustingNumOFProudct

---
+++ RAQ-Store/RAQ-Store/Migrations/202005150212293_defult-products.cs
@@ -0,0 +1,18 @@
+﻿namespace RAQ_Store.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class defultproducts : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Category", "number_of_products", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Category", "number_of_products", c => c.Int());
+        }
+    }
+}
